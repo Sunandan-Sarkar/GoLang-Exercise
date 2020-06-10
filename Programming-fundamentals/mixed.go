3aa682e575ded3b7ca6f91a41b69861d05daf025
Sunandan-Sarkar <sarkarsunandan87@gmail.com>
2020-06-10 14:34:47 +0300

    You are doing Great

---
+++ Programming-fundamentals/mixed.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x int
+
+func main() {
+	x = 214748364999
+	fmt.Println(x)
+
+}
--- Programming-fundamentals/numerictypes.go
+++ Programming-fundamentals/numerictypes.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 )
 
-func main(){
-	x:=42
-	y:=42.768765
-	fmt.Println(x)
-	fmt.Printf("%T\n",x)
-	fmt.Println(y)
-	fmt.Printf("%T\n",y)
-}
+var x int
+var y float64
 
+func main() {
+	//	x:=42
+	//	y:=42.768765 or below assigned way
+	x = 42
+	y = 42.6566555
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+}
