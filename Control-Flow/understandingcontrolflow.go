94a4237d58e5d73a10511eec089825eeaf3451ce
Sunandan-Sarkar <sarkarsunandan87@gmail.com>
2020-06-11 20:43:32 +0300

    You are doing Great

--- Control-Flow/understandingcontrolflow.go
+++ Control-Flow/understandingcontrolflow.go
@@ -1,9 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
-func main(){
-	
-}
+import "fmt"
 
+func main() {
+	//for init;condition;post{} this is the law, only For
+	for i := 0; i < 100; i++ {
+		fmt.Println(i)
+	}
+
+}
