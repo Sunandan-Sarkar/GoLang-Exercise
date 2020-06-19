122d326049724853280bcddf1258f00738b35063
Sunandan-Sarkar <sarkarsunandan87@gmail.com>
2020-06-19 22:28:10 +0300

    You are doing Great

--- Exercise-1/forloop2.go
+++ Exercise-1/forloop2.go
@@ -7,7 +7,7 @@ import (
 func main() { //for { }
 	p := 0
 	for {
-		if p > 5 {// i feel if statement is a condition
+		if p > 5 { // i feel if statement is a condition
 			break
 		}
 		fmt.Println(p)
--- Exercise-1/forloopex1.go
+++ Exercise-1/forloopex1.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() { //for condition { }
-	i := 8 //"initialization" can be before for
+	i := 8 //"initialization" can be before "for"
 	for i <= 10 {
 		fmt.Println(i)
 		i++ //"post" can be after println
