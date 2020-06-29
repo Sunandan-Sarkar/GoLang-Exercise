faeb461441e1aaefd0f529d90d200077fa5fff67
Sunandan-Sarkar <sarkarsunandan87@gmail.com>
2020-06-29 18:42:14 +0300

    You are doing Great

--- Functions/Conversions.go
+++ Functions/Conversions.go
@@ -9,9 +9,9 @@ type rocket int
 func main() {
 	var x rocket = 33
 	fmt.Println(x)
-	fmt.Printf("%T",x)
+	fmt.Printf("%T", x)
 	var y string
-	y=string(x)
+	y = string(x)
 	fmt.Println(y)
-	fmt.Printf("%T",y)
+	fmt.Printf("%T", y)
 }
