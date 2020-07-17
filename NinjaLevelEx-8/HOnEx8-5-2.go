fda5bd55cd4193fc652efcd7f490307fa820f97a
Sunandan-Sarkar <sarkarsunandan87@gmail.com>
2020-07-17 12:56:10 +0300

    You are doing Great

--- NinjaLevelEx-8/HOnEx8-5-2.go
+++ NinjaLevelEx-8/HOnEx8-5-2.go
@@ -5,20 +5,26 @@ import (
 	"sort"
 )
 
-type ur struct {
+type us struct {
 	First   string
 	Last    string
 	Age     int
 	Sayings []string
 }
-type ByLast []ur
+type ByAge []us
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+type ByLast []us
 
 func (bl ByLast) Len() int           { return len(bl) }
 func (bl ByLast) Swap(i, j int)      { bl[i], bl[j] = bl[j], bl[i] }
 func (bl ByLast) Less(i, j int) bool { return bl[i].Last < bl[j].Last }
 
 func main() {
-	u1 := ur{
+	u1 := us{
 		First: "James",
 		Last:  "Bond",
 		Age:   32,
@@ -29,7 +35,7 @@ func main() {
 		},
 	}
 
-	u2 := ur{
+	u2 := us{
 		First: "Miss",
 		Last:  "Moneypenny",
 		Age:   27,
@@ -40,7 +46,7 @@ func main() {
 		},
 	}
 
-	u3 := ur{
+	u3 := us{
 		First: "M",
 		Last:  "Hmmmm",
 		Age:   54,
@@ -50,12 +56,33 @@ func main() {
 			"Can someone please tell me where James Bond is?",
 		},
 	}
-
-	users := []ur{u1, u2, u3}
+	fmt.Println("------------------")
+	users := []us{u1, u2, u3}
+	for i,v:=range users {
+		fmt.Println("People #", i)
+		fmt.Println("\t", v.First, v.Last, v.Age)
+		for _, v2 := range v.Sayings {
+			fmt.Println("\t", v2)
+		}
+	}
+	fmt.Println("------------------")
+	sort.Sort(ByAge(users))
+	for i,v:=range users{
+		fmt.Println("\t People #",i)
+		fmt.Println("\t\t",v.First,v.Last,v.Age)
+		for _,v2:=range v.Sayings{
+			fmt.Println("\t",v2)
+		}
+	}
+	fmt.Println("------------------")
 	sort.Sort(ByLast(users))
-	fmt.Println(users)
-
+	for i,v:=range users{
+		fmt.Println("People #", i)
+		fmt.Println("\t",v.First,v.Last)
+		for _,v2:=range v.Sayings{
+			fmt.Println("\t",v2)
+		}
+	}
 
 }
-
 
