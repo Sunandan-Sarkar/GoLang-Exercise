c3c462942ed9829c1cfcbaf2d327601958540297
Sunandan-Sarkar <sarkarsunandan87@gmail.com>
2020-07-20 22:15:47 +0300

    You are doing Great

---
+++ Concurrency/WaitGroup/wg1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	fmt.Println("OS:\t", runtime.GOOS)
+	fmt.Println("ARCH:\t", runtime.GOARCH)
+	fmt.Println("CPUs:\t", runtime.NumCPU())
+	fmt.Println("Gorutine:", runtime.NumGoroutine())
+	wg.Add(1)
+	go foo()
+	bar()
+	fmt.Println("CPUs:\t", runtime.NumCPU())
+	fmt.Println("Gorutine:", runtime.NumGoroutine())
+	wg.Wait()
+}
+
+func foo() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Foo:", i)
+	}
+	wg.Done()
+}
+func bar() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Bar:", i)
+	}
+}
