990614f0ef84913f40bcbd13ce96c1d82b0f179d
Sunandan-Sarkar <sarkarsunandan87@gmail.com>
2020-07-29 11:57:51 +0300

    select receive channels

---
+++ Channels/Exercise/myex5.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main(){
+	f:=make(chan int)
+	b:=make(chan int)
+
+	go func() {
+		for i:=0;i<5000;i++{
+			f<-i
+		}
+	}()
+
+	go func() {
+		for i:=0;i<5000;i++{
+			b<-i
+		}
+	}()
+	foo(f,b)
+	fmt.Println("About to exit")
+}
+func foo(f,b chan int){
+for i:=0;i<10000;i++{
+	select {
+	case v:= <-f:
+		fmt.Println("From Foo:",v)
+		case v:= <-b :
+			fmt.Println("From Bar:",v)
+	}
+}
+fmt.Println("About to exit too")
+}
